369a0d7549932cb840bfa518896c1f214c306c54
mmacdougall <mmacdougall@etsy.com>
2018-12-11 00:27:00 -0500

    [Day 11] Part 1 Solution

--- AdventOfCode.playground/Pages/2018 Day 11.xcplaygroundpage/Contents.swift
+++ AdventOfCode.playground/Pages/2018 Day 11.xcplaygroundpage/Contents.swift
@@ -2,10 +2,60 @@
 
 import Foundation
 
-func linesFromFilename(_ filename: String) -> [String] {
-    let fileURL = Bundle.main.url(forResource: filename, withExtension: "txt")
-    let string = (try? String(contentsOf: fileURL!, encoding: .utf8)) ?? ""
-    return string.split(separator: "\n").map { String($0) }
+struct Grid {
+    typealias Position = (x: Int, y: Int)
+//    struct FuelCell {
+//        let
+//    }
+    private static let min = 1
+    private static let max = 300
+    let serialNumber: Int
+    
+
+    func powerLevelAtPosition(position: Position) -> Int {
+        let rackId = position.x + 10
+        let step1 = rackId * position.y
+        let step2 = step1 + serialNumber
+        let step3 = step2 * rackId
+        let step4 = step3 / 100 % 10
+        return step4 - 5
+    }
+    
+    func getPositionOfMaxPowerLevel() -> Position {
+        var max = 0
+        var position = (-1, -1)
+        for x in stride(from: Grid.min, to: Grid.max, by: 1) {
+            for y in stride(from: Grid.min, to: Grid.max, by: 1) {
+                let positions = [
+                    (x, y),
+                    (x, y+1),
+                    (x, y+2),
+                    (x+1, y),
+                    (x+1, y+1),
+                    (x+1, y+2),
+                    (x+2, y),
+                    (x+2, y+1),
+                    (x+2, y+2),
+                ]
+                let power = positions.map {
+                    return powerLevelAtPosition(position: $0)
+                }.reduce(0, +)
+                if power > max {
+                    max = power
+                    position = (x, y)
+                }
+            }
+        }
+        print(max)
+        return position
+    }
+    
 }
 
+
+let grid = Grid(serialNumber: 7989)
+let position = grid.getPositionOfMaxPowerLevel()
+print(position)
+//let power = grid.powerLevelAtPosition(x: 101, y: 153)
+//power
 //: [Next](@next)
